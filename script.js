58b26f241ae11bb66e1ebfb599e32191e93ca1a7
yadhukkk <96060556+yadhukkk@users.noreply.github.com>
2021-12-16 16:38:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("mynav");
     if (x.className === "header-col2 topnav") {
       x.className += " responsive header-col2";
     } else {
